71dc52107e9b8ff3c4cb4ace51e1eb759adee73b
oasissegar <oasissegar@gmail.com>
2025-01-15 08:15:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ query_params = st.experimental_get_query_params()
 if 'secret' in query_params:
     secret_key = query_params['secret'][0]  # Ambil nilai secret dari URL
     token = generate_totp_token(secret_key)
-    st.success(f'OTP Anda: {token}')
+    
+    # Menampilkan OTP di tengah dengan ukuran font yang besar menggunakan HTML
+    st.markdown(f"""
+        <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+            <h1 style="font-size: 50px; font-weight: bold; color: green;">OTP Anda: {token}</h1>
+        </div>
+    """, unsafe_allow_html=True)
 else:
     st.error("Tidak ada secret key di URL. Harap tambahkan secret key pada query parameter.")
