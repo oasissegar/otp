3dce619f769c824755fa98277769c43f1c3e8dd5
oasissegar <oasissegar@gmail.com>
2025-01-15 08:34:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def generate_totp_token(secret):
     
     return token
 
-# Ambil parameter 'secret' dari URL
+st.set_page_config(page_title="Master Media OTP", page_icon="🔑")
 query_params = st.experimental_get_query_params()
 
 # Cek apakah parameter 'secret' ada
