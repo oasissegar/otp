66391b1d67ff88b530edce5d296865fd674d563e
oasissegar <oasissegar@gmail.com>
2025-01-15 08:16:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ def generate_totp_token(secret):
     
     return token
 
-# Streamlit application layout
-st.title("Master Media OTP Generator")
-
 # Ambil parameter 'secret' dari URL
 query_params = st.experimental_get_query_params()
 
@@ -23,7 +20,7 @@ if 'secret' in query_params:
     
     # Menampilkan OTP di tengah dengan ukuran font yang besar menggunakan HTML
     st.markdown(f"""
-        <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+        <div style="display: flex; justify-content: center; align-items: center; height: 50vh;">
             <h1 style="font-size: 50px; font-weight: bold; color: green;">OTP Anda: {token}</h1>
         </div>
     """, unsafe_allow_html=True)
